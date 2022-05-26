c50eaed4b61a647c5d6c08b3d262452f1a740b8d
Xstar97 <Xstar97@users.noreply.github.com>
2022-05-26 11:31:42 -0400

    add error pages

---
+++ 404/cave-men/script.js
@@ -0,0 +1,7 @@
+// no JS this time !
+let hostname = location.hostname;
+hostname = hostname.substring(hostname.indexOf(".") + 1);
+window.addEventListener('load', (event) => {
+var a = document.getElementById('home'); 
+a.href = "https://" + hostname;
+});
